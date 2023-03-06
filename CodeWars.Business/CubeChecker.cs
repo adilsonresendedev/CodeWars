19af95862c4e84dc36e0637d40837292acb3a9bf
adilsonresende <adilsonresendetp@gmail.com>
2023-03-06 00:25:09 -0300

    Add project files.

---
+++ CodeWars.Business/CubeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Business
+{
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            int powValue = 3;
+            bool result;
+
+            if (volume <= 0 || side <= 0)
+            {
+                result = false;
+                return result;
+            }
+
+            double expectedVolume = Math.Pow(side, powValue);
+            result = volume == expectedVolume;
+
+            return result;
+        }
+    }
+}
---
+++ CodeWars.UnitTests/CubeCheckerTests.cs
@@ -0,0 +1,26 @@
+﻿using CodeWars.Business;
+using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    public class CubeCheckerTests
+    {
+        [Test]
+        public void ShouldReturnFalse()
+        {
+            CubeChecker cube = new CubeChecker();
+            Assert.That(cube.IsCube(1, 1), Is.EqualTo(true));
+            Assert.That(cube.IsCube(8, 2), Is.EqualTo(true));
+        }
+        [Test]
+        public void ShouldReturnTrue()
+        {
+            CubeChecker cube = new CubeChecker();
+            Assert.That(cube.IsCube(2, 1), Is.EqualTo(false));
+            Assert.That(cube.IsCube(6, 3), Is.EqualTo(false));
+            Assert.That(cube.IsCube(-8, -2), Is.EqualTo(false));
+            Assert.That(cube.IsCube(0, 0), Is.EqualTo(false));
+            Assert.That(cube.IsCube(200, 4), Is.EqualTo(false));
+        }
+    }
+}
